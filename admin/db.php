dc9f02b57ede0dce9cad5fa4bc3d55d186db5444
6Anvesh <33061952+6Anvesh@users.noreply.github.com>
2017-11-17 01:43:20 +0530

    Add files via upload

---
+++ admin/db.php
@@ -0,0 +1,7 @@
+<?php
+$server="localhost";
+$username="root";
+$password="root";
+$db ="placement";
+$con=mysqli_connect($server, $username, $password, $db);
+?>
---
+++ admin/header.php
@@ -0,0 +1,18 @@
+ <nav  class="navbar navbar-inverse">
+		 <div class="navbar-fluid">
+			 <ul class="nav navbar-nav">
+			    
+	                        <li><a href="admin.php">person detalis</a></li>
+				<li><a href="companies.php">companys</a></li>
+				<li><a href="register.php">Register</a></li>
+				<li><a href="contact.php">contacts</a></li>
+			 </ul>
+			 <div class="navbar-header navbar-right">
+			
+			 <div class="navbar-brand"> <a href="../index.php">Home Page</a></div>
+				<div class="navbar-brand">Admin Panal</div>
+				</div>
+			 </div>
+			 
+		
+		 </nav> 
